049feca3cf9bdb08fb068772ea3a094fe3e9fd5f
SidD11111 <79270579+SidD11111@users.noreply.github.com>
2025-03-09 23:57:44 -0500

    added flask backend structure

---
+++ server/routes.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, request, jsonify, render_template
+from server.auth import require_api_key
+import secrets
+
+routes = Blueprint('routes', __name__)
+
+#Endpoint to generate new API keys
+@routes.post('/register')
+def generate_api_key():
+    api_key = secrets.token_hex(32)
+
+    try:
+        pass # UPDATE: Store API key in Postgres DB
+
+        return jsonify({"message": "API key generated successfully", "api_key": api_key}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+#Endpoint to generate new API keys
+@routes.post('/login')
+def register():
+    # Contains logic for logging in a user
+    pass
+    
+
+#Endpoint for adding points
+@routes.post('/findSong')
+@require_api_key 
+def getSong():
+    # Contains logic for getting song data from Spotify API
+    pass
+   
+
+#Endpoint for returning balance
+@routes.get('/updateProfile')
+@require_api_key
+def get_balance():
+    # Contains logic for sending user data to DB
+    pass
